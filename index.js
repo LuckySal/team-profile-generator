f4aa20227df2271073df2ff16ab25d73ee2a072e
Brandon <hillbp2014@gmail.com>
2022-10-27 21:27:55 -0400

    did a whole lot of stuff

--- index.js
+++ index.js
@@ -1,31 +1,208 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const path = require('path');
-const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
-const DIST_DIR = path.resolve(__dirname, 'dist');
-const distPath = path.join(DIST_DIR, 'team.html');
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distPath = path.join(DIST_DIR, "team.html");
 
-const render = require('./src/page-template.js');
+const render = require("./src/page-template.js");
+const { inherits } = require("util");
 
 const teamMembers = [];
 
+async function init() {
+    await addManager(teamMembers);
+    let addMember;
+    do {
+        addMember = await addNewMember(teamMembers);
+    } while (addMember);
+    let contents = render(teamMembers);
+    let filename = "index.html";
+    let i = 1;
+    let isDuplicateFile;
+    do {
+        try {
+            fs.accessSync(`./dist/${filename}`, fs.constants.F_OK);
+            isDuplicateFile = true;
+            filename = `index(${i++}).html`;
+        } catch (err) {
+            isDuplicateFile = false;
+        }
+    } while (isDuplicateFile);
+    await writeToFile(filename, contents);
+}
 
-// function for creating manager - inquirer questions
-  // take those questions and create a new Manager with the user provided answers
-  // push that new Manager to the team members array
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFileSync(`./dist/${fileName}`, data);
+        console.log(`Created file ${fileName} successfully.`);
+    });
+}
 
-  // follow the same pattern for each type of employee
-  // build a function for them that uses inquirer
+function addManager(team) {
+    return new Promise((resolve, reject) => {
+        const questions = [
+            {
+                type: "input",
+                message: "What is the manager's name?",
+                name: "name",
+            },
+            {
+                type: "number",
+                message: "What is the office number?",
+                name: "office",
+            },
+            {
+                type: "number",
+                message: "What is the manager's Employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the manager's email address?",
+                name: "email",
+            },
+        ];
+        inquirer
+            .prompt(questions)
+            .then((answers) => {
+                const manager = new Manager(
+                    answers.name,
+                    answers.id,
+                    answers.email,
+                    answers.office
+                );
+                team.push(manager);
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
+function addEngineer(team) {
+    return new Promise((resolve, reject) => {
+        const questions = [
+            {
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "name",
+            },
+            {
+                type: "number",
+                message: "What is the engineer's Employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's GitHub username?",
+                name: "github",
+            },
+        ];
+        inquirer
+            .prompt(questions)
+            .then((answers) => {
+                const engineer = new Engineer(
+                    answers.name,
+                    answers.id,
+                    answers.email,
+                    answers.github
+                );
+                team.push(engineer);
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
-// STRUCTURING IT
+function addIntern(team) {
+    return new Promise((resolve, reject) => {
+        const questions = [
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "name",
+            },
+            {
+                type: "number",
+                message: "What is the intern's Employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the intern's email address?",
+                name: "email",
+            },
 
-// start with manager function, since every team needs a manager
-// at the end of manager function, call a createTeam function
+            {
+                type: "input",
+                message: "What is the intern's school?",
+                name: "school",
+            },
+        ];
+        inquirer
+            .prompt(questions)
+            .then((answers) => {
+                const intern = new Intern(
+                    answers.name,
+                    answers.id,
+                    answers.email,
+                    answers.school
+                );
+                team.push(intern);
+                resolve();
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
-// this function would simply ask the user which type of employee they would like to add, based on their choice, run the correesponding function
+async function addNewMember(team) {
+    return new Promise((resolve, reject) => {
+        const questions = [
+            {
+                type: "list",
+                message: "Would you like to add a team member?",
+                name: "member",
+                choices: ["Engineer", "Intern", "No more"],
+            },
+        ];
+        inquirer
+            .prompt(questions)
+            .then(async (answers) => {
+                if (answers.member === "No more") {
+                    resolve(false);
+                } else {
+                    switch (answers.member) {
+                        case "Engineer":
+                            await addEngineer(team);
+                            resolve(true);
+                            break;
+                        case "Intern":
+                            await addIntern(team);
+                            resolve(true);
+                            break;
+                        default:
+                            reject(new Error("Something went wrong"));
+                    }
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
-// at the end, use fs to write file while sending the team array over to the function you brought in from page-template.js
+init();
--- src/page-template.js
+++ src/page-template.js
@@ -1,29 +1,106 @@
-const generateTeam = team => {
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 
+const generateTeam = (team) => {
     // create the manager html
-    const generateManager = manager => {
-        return `
-        
-        `;
+    const generateManager = (manager) => {
+        return `<div class="col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center">Manager</h5>
+                <h6 class="card-title text-center">${manager.getName()}</h6>
+                <ul class="list-group">
+                    <li class="list-group-item">Employee ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
     };
 
     // create the html for engineers
-    const generateEngineer = engineer => {
-        return `
-
-        `;
+    const generateEngineer = (engineer) => {
+        return `<div class="col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center">Engineer</h5>
+                <h6 class="card-title text-center">${engineer.getName()}</h6>
+                <ul class="list-group">
+                    <li class="list-group-item">Employee ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub Username: ${engineer.getGithubUsername()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
     };
 
     // create the html for interns
-    const generateIntern = intern => {
-        return `
-
-        `;
+    const generateIntern = (intern) => {
+        return `<div class="col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center">Intern</h5>
+                <h6 class="card-title text-center">${intern.getName()}</h6>
+                <ul class="list-group">
+                    <li class="list-group-item">Employee ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
     };
 
     // use the team array to generate pieces of html based on the employee role
-}
+    let body = "";
+    team.forEach(element => {
+        if (element instanceof Manager) {
+            body += generateManager(element);
+        } else if (element instanceof Engineer) {
+            body += generateEngineer(element);
+        } else if (element instanceof Intern) {
+            body += generateIntern(element);
+        }
+    });
+    return body;
+};
 
-module.exports = team => {
+module.exports = (team) => {
     // template literal - html body that calls the generate team function
-}
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+                crossorigin="anonymous" />
+            <link rel="stylesheet" href="style.css" />
+            <title>Team</title>
+        </head>
+        <body>
+            <header>
+                <h1 class="text-center">My Team</h1>
+            </header>
+            <main class="container">
+                <div class="row">
+                    ${generateTeam(team)}
+                </div>
+            </main>
+            <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+                crossorigin="anonymous"></script>
+        </body>
+    </html>
+    `;
+};
